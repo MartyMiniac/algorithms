fd5dfdce4bdfddb5340771b6a0c201fc9c072abf
Rohan Verma <rohanvermaara@gmail.com>
2021-05-21 18:49:24 +0530

    corrected the insertion sort algo

--- searching/insertionSort.java
+++ searching/insertionSort.java
@@ -4,30 +4,27 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String args[]) {
-        int arr[] = arrayPooper(5, 5);
-
+        //populate arr[] with some random numbers
+    	int arr[] = arrayPooper(5, 5);
+    	
+    	//initial array display
         System.out.println(Arrays.toString(arr));
 
-        for(int i=0; i<arr.length-1; i++) {
-            // System.out.println("----------------------");
-            // System.out.println("Chance for "+arr[i+1]);
-            for(int j=i+1; j>0; j--) {
-                if(arr[j]<arr[j-1]) {
-                    arr=swap(arr, j, j-1);
-                    // System.out.print("Swap ");
-                }
-                // System.out.println(Arrays.toString(arr));
-            }
+        //consider array below i-1 is sorted
+        //we are taking each element from the array and comparing to the sorted array
+        //where the comparison becomes false just before that element the key is added and rest are shift to make space for the key.
+        for(int j=1; j<arr.length; j++) {
+        	int key=arr[j];
+        	int i=j-1;
+        	while(i>=0 && arr[i]>key) {
+        		//shift operation for the key's possible place
+        		arr[i+1]=arr[i];
+        		i--;
+        	}
+        	arr[i+1]=key;
         }
 
         System.out.println(Arrays.toString(arr));
-    }
-
-    private static int[] swap(int[] arr, int i, int j) {
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-        return arr;
     }
 
     private static int[] arrayPooper(int size, int range) {
